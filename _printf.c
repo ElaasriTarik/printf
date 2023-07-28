a62ba8921f6e8d2ebcfc49f6534ec2dda0109c52
Abdessadek EL AASRI <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-28 16:07:03 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,35 +7,29 @@
  *
  * Return: Number chars Printed
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list list;
-
+int cc = 0;
+va_list list;
 va_start(list, format);
 if (!format || (format[0] == '%' && !format[1]))
-	{
 		return (-1);
-	}
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
 		return (-1);
-	}
 	while (format && *format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			count += switche(list, *format);
+			cc += switche(list, *format);
 		}
 		else
 		{
 			_putchar(*format);
-			count++;
+			cc;
 		}
 		format++;
 	}
 	va_end(list);
-	return (count);
+	return cc;
 }
