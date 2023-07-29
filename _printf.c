b5f7c7bfc5a67a70aae77331b8a5842f379ffb0e
Abdessadek EL AASRI <37253593+ElaasriTarik@users.noreply.github.com>
2023-07-29 03:11:33 +0100

    updated

--- _printf.c
+++ _printf.c
@@ -1,37 +1,38 @@
 #include "main.h"
 
 /**
- * _printf - Prints stuff
- * @format: Format of given input
- * Return: Number characters Printed
+ * _printf - Prints string according to format
+ * @format: format
+ * Return: Number characters printed
  */
+
 int _printf(const char *format, ...)
 {
-int cc = 0;
+int count = 0;
 va_list list;
 va_start(list, format);
-if (!format || (format[0] == '%' && !format[1]))
-{
+if (format == NULL || (format[0] == '%' && format[1] == NULL))
+	{
 		return (-1);
-}
+	}
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-{		
+	{
 		return (-1);
-}
+	}
 	while (format && *format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			cc += switche(list, *format);
+			count += switche(list, *format);
 		}
 		else
 		{
 			_putchar(*format);
-			cc;
+			count++;
 		}
 		format++;
 	}
 	va_end(list);
-	return cc;
+	return (count);
 }
