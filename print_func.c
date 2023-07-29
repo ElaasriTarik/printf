3d7f6bb05af64b0e9489d0cba93fa707ef960a24
ElaasriTarik <elaasriya2003@gmail.com>
2023-07-29 03:27:32 +0100

    update

--- print_func.c
+++ print_func.c
@@ -9,8 +9,8 @@
 
 int switche(va_list list, char format)
 {
-	int cc = 0, i = 0;
-	string fun[] = {
+int cc = 0, i = 0;
+string fun[] = {
 		{'c', print_chare},
         {'i', print_integer},
         {'d', print_integer},
@@ -22,7 +22,7 @@ int switche(va_list list, char format)
 		{'S', print_str},
         {'p', print_add},
         {'\0', NULL}
-	};
+};
 
 	i = 0;
 	while ((format != fun[i].str) && (fun[i].str != '\0'))
@@ -37,12 +37,12 @@ int switche(va_list list, char format)
 		_putchar('%');
 		cc++;
 	}
+	
 	else
 	{
 		_putchar('%');
 		_putchar(format);
 		cc += 2;
 	}
-
-	return (cc);
+return (cc);
 }
--- print_string.c
+++ print_string.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * print_str - printf special char in hex
  * @arg: Argument
@@ -9,34 +7,32 @@
 
 int print_str(va_list arg)
 {
-	char *str = va_arg(arg, char *);
-	int  i = 0;
-	int count = 0;
+char *string = va_arg(arg, char *);
+int  i = 0;
+int count = 0;
 
-	while (str[i] != '\0')
-	{
-		if (search(str[i]) != -1)
-		{
-			_putchar(str[i]);
-			count++;
-		}
-		else
-		{
-			_putchar('\\');
-			_putchar('x');
-			count += 2;
-			if (str[i] < 16)
-			{
-				_putchar('0');
-				count += _printf("%X", str[i]);
-				count++;
-			}
-			else
-			{
-				count += _printf("%X", str[i]);
-			}
-		}
-		i++;
-	}
-	return (count);
+while (string[i] != '\0')
+{
+if (search(string[i]) != -1)
+{
+_putchar(string[i]);
+count++;
+}
+else
+{
+_putchar('\\');
+_putchar('x');
+count += 2;
+if (string[i] < 16)
+{
+_putchar('0');
+count += _printf("%X", string[i]);
+count++;
+}
+else
+count += _printf("%X", string[i]);
+}
+i++;
+}
+return (count);
 }
